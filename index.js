8e1f418a28b8096584565d580c5a941baa043f59
Hrach Sahakyan <62832144+aacc1on@users.noreply.github.com>
2025-07-23 17:22:21 +0400

    push

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: process.env.FRONTEND_URL;
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }));
 app.use(express.json());
